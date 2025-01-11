8321328411156035676509129062979cd70ad323
muley2525 <122787182+muley2525@users.noreply.github.com>
2025-01-11 11:23:12 +0530

    Update example.tf

--- example.tf
+++ example.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region     = "ap-south-1"
 }
 
-resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/8"
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+
+  tags = {
+    Name = "myVPC"
+  }
 }
